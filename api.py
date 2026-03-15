96d9de21f46657ec57b0a6ba2dd253578c162b49
s varshini <varshinijeevi2005@gmail.com>
2026-03-15 10:55:17 +0530

    Fix deployment

--- api.py
+++ api.py
@@ -1,12 +1,11 @@
 import tensorflow as tf
 import numpy as np
 from flask import Flask, request, jsonify
-from tensorflow.keras.preprocessing import image
-import io
 from PIL import Image
 
 app = Flask(__name__)
 
+# Load model
 model = tf.keras.models.load_model("waste_classifier.keras")
 
 class_names = ["Hazardous","Non-Recyclable","Organic","Recyclable"]
@@ -21,7 +20,7 @@ def predict():
     file = request.files["image"]
     img = Image.open(file.stream).resize((224,224))
 
-    img_array = np.array(img)/255.0
+    img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
     prediction = model.predict(img_array)
@@ -29,5 +28,7 @@ def predict():
 
     return jsonify({"prediction": result})
 
+
+# only used for local testing
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
